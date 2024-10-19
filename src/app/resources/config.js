93c68c0921ad6d0da88a7dd4ae18a0364ae0e5e8
Tiago Soriano <tiagosoriano2000@gmail.com>
2024-10-19 17:39:44 -0300

    adjust some places

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -1,58 +1,68 @@
-const baseURL = 'demo.app'
+const baseURL = "tiagosoriano.dev";
 
 // Enable localization
 const i18n = false;
 
 // Manage localized content in the messages folder
 const i18nOptions = {
-    locales: ['en'],            // A list of all locales that are supported, e.g. ['en','id']
-    defaultLocale: 'en'         // Locale used by default and as a fallback
-}
+  locales: ["en"], // A list of all locales that are supported, e.g. ['en','id']
+  defaultLocale: "en", // Locale used by default and as a fallback
+};
 
 const routes = {
-    '/':        true,
-    '/about':   true,
-    '/work':    true,
-    '/blog':    true,
-    '/gallery': true,
-}
+  "/": true,
+  "/about": true,
+  "/work": true,
+  "/blog": true,
+  "/gallery": false,
+};
 
 // Enable password protection on selected routes
 // Set password in pages/api/authenticate.ts
 const protectedRoutes = {
-    '/work/automate-design-handovers-with-a-figma-to-code-pipeline': true
-}
+  "/work/automate-design-handovers-with-a-figma-to-code-pipeline": true,
+};
 
 const effects = {
-    gradient: true,
-    dots:     true,
-    lines:    false,
-}
-
+  gradient: true,
+  dots: true,
+  lines: false,
+};
+// #143dac
 const style = {
-    theme:       'dark',         // dark | light
-    neutral:     'gray',         // sand | gray | slate
-    brand:       'aqua',         // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-    accent:      'yellow',       // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-    solid:       'contrast',     // color | contrast
-    solidStyle:  'flat',         // flat | plastic
-    border:      'playful',      // rounded | playful | conservative
-    surface:     'translucent',  // filled | translucent
-    transition:  'all'           // all | micro | macro
-}
+  theme: "dark", // dark | light
+  neutral: "gray", // sand | gray | slate
+  brand: "aqua", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  accent: "cyan", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  solid: "contrast", // color | contrast
+  solidStyle: "flat", // flat | plastic
+  border: "playful", // rounded | playful | conservative
+  surface: "translucent", // filled | translucent
+  transition: "all", // all | micro | macro
+};
 
 const display = {
-    location: true,
-    time:     true
-}
+  location: true,
+  time: false,
+};
 
 const mailchimp = {
-    action: 'https://url/subscribe/post?parameters',
-    effects: {
-        gradient: true,
-        dots:     false,
-        lines:    true,
-    }
-}
+  action: "https://url/subscribe/post?parameters",
+  effects: {
+    gradient: true,
+    dots: false,
+    lines: true,
+  },
+};
 
-export { routes, protectedRoutes, effects, style, display, mailchimp, baseURL, i18n, i18nOptions };
+export {
+  routes,
+  protectedRoutes,
+  effects,
+  style,
+  display,
+  mailchimp,
+  baseURL,
+  i18n,
+  i18nOptions,
+};
