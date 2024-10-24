78ab8f8ef0553012e4220a1892534604cdf206da
Tiago Soriano <tiagosoriano2000@gmail.com>
2024-10-24 01:50:46 -0300

    update

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,16 +1,20 @@
 import Script from "next/script";
 
+export const isProduction = process.env.NODE_ENV === "production";
+
 // INFO: https://stackoverflow.com/questions/60411351/how-to-use-google-analytics-with-next-js-app
 const GoogleAnalytics = () => {
   return (
     <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
-      />
-      {/* Initialize GA4 */}
-      <Script id="ga4-init" strategy="afterInteractive">
-        {`
+      {isProduction && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+            strategy="afterInteractive"
+          />
+          {/* Initialize GA4 */}
+          <Script id="ga4-init" strategy="afterInteractive">
+            {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           
@@ -18,9 +22,12 @@ const GoogleAnalytics = () => {
           
           gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
             page_path: window.location.pathname,
+            anonymize_ip: true,
           });
         `}
-      </Script>
+          </Script>
+        </>
+      )}
     </>
   );
 };
